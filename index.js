4884d5ac8f0c5c63c16690db9453f4256599ff0e
webster-rafael <webster.rafael2014@gmail.com>
2024-10-13 16:40:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Rota para calcular frete
+app.post("/calculate-frete", async (req, res) => {
+  const { cep } = req.body;
+
+  const url = "https://melhorenvio.com.br/api/v2/me/shipment/calculate";
+  const token = process.env.TOKEN_ACESS;
+
+  const data = {
+    from: {
+      postal_code: "79140000", // CEP de origem fixo
+    },
+    to: {
+      postal_code: cep, // CEP recebido do cliente
+    },
+    package: {
+      height: 75,
+      width: 100,
+      length: 7,
+      weight: 1,
+    },
+  };
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    res.json(response.data); // Retorna os dados da resposta para o frontend
+  } catch (error) {
+    console.error("Erro ao calcular frete:", error);
+    res.status(500).json({ error: "Erro ao calcular frete." });
+  }
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
